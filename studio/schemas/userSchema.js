6b06a71ef8d46d9a9c520029888c1f04b9a66331
AayushMohan <aayushmohan1702@gmail.com>
2022-04-16 19:58:36 +0530

    Added Sanity and Schema

---
+++ studio/schemas/userSchema.js
@@ -0,0 +1,22 @@
+export const userSchema = {
+  name: 'user',
+  type: 'document',
+  title: 'Users',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'walletAddress',
+      type: 'string',
+      title: 'Wallet Address',
+    },
+    {
+      name: 'profileImage',
+      type: 'image',
+      title: 'Profile Image',
+    },
+  ],
+}
